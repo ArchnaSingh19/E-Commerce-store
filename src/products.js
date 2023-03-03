0ad34d698a8b784195dc37b1664de41a22b3dda4
ArchnaSingh19 <singharclion@gmail.com>
2023-03-03 20:08:42 +0530

    updated

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/apple 129.jpg';
+import Product2 from './assets/asus 78.png';
+import product3 from './assets/del.jpg';
+import product4 from './assets/lenovo 45.jpg';
+import product5 from './assets/samsung 54.jpg';
+import product6 from './assets/macbook-pro.webp';
+import product7 from './assets/lenovo 129.webp';
+import product8 from './assets/aser 56.webp';
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Apple Laptop",
+        price: 129000,
+        productImage:product1,
+    },
+    {
+        id:2,
+        productName: "Asus laptop",
+        price: 78000,
+        productImage: Product2,
+    },
+    {
+        id:3,
+        productName: "Del Laptop",
+        price: 175000,
+        productImage:product3,
+    },
+    {
+        id:4,
+        productName: "lenovo Laptop",
+        price: 45000,
+        productImage:product4,
+    },
+    {
+        id:5,
+        productName: "Samsung Laptop",
+        price: 82000,
+        productImage: product5
+    },
+    {
+        id:6,
+        productName: "macbook-pro",
+        price: 235000,
+        productImage: product6,
+    },
+    {
+        id:7,
+        productName: "Lenovo",
+        price: 89000,
+        productImage: product7,
+    },
+    {
+        id:8,
+        productName: "Aser Laptop",
+        price: 56000,
+        productImage: product8,
+    }
+];
